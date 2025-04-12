2f71f6016d91652ee1a0fd71c4a450feb14d7df0
Lejitop <alejocrrs@gmail.com>
2025-04-12 16:53:37 -0500

    Initialize EPharma Spring Boot REST API with Gradle

---
+++ src/main/java/com/lejito/epharma/EPharmaApplication.java
@@ -0,0 +1,13 @@
+package com.lejito.epharma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EPharmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EPharmaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lejito/epharma/EpharmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lejito.epharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpharmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
