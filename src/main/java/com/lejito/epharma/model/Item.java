5368b2c9d1a96785e93119d54cbef23ff54d65c9
Lejitop <alejocrrs@gmail.com>
2025-04-12 17:19:25 -0500

    feat: Add constructor to Item class

--- src/main/java/com/lejito/epharma/model/Item.java
+++ src/main/java/com/lejito/epharma/model/Item.java
@@ -7,6 +7,11 @@ public class Item {
     private Medicine medicine;
     private int quantity;
 
+    public Item(Medicine medicine, int quantity) {
+        this.medicine = medicine;
+        this.quantity = quantity;
+    }
+
     float calculatePrice() {
         return medicine.calculatePrice(quantity);
     }
