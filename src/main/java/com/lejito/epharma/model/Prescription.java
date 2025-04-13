a7d0d3fd8cedc7054dd9a1c1cb3c4a284fd864ae
Lejitop <alejocrrs@gmail.com>
2025-04-12 22:54:46 -0500

    refactor: Add constructor in Prescription class

--- src/main/java/com/lejito/epharma/model/Prescription.java
+++ src/main/java/com/lejito/epharma/model/Prescription.java
@@ -10,4 +10,13 @@ public class Prescription {
     private int quantity;
     private Doctor prescribedBy;
     private boolean available;
+
+    public Prescription(int id, Patient patient, Medicine medicine, int quantity, Doctor prescribedBy) {
+        this.id = id;
+        this.patient = patient;
+        this.medicine = medicine;
+        this.quantity = quantity;
+        this.prescribedBy = prescribedBy;
+        this.available = true;
+    }
 }
