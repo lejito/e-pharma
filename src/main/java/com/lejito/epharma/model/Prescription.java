bbe1421da9022b3570c29a7836fae2d07d23dadd
Lejitop <alejocrrs@gmail.com>
2025-04-12 22:57:05 -0500

    refactor: Remove prescribedBy parameter from Prescription class

--- src/main/java/com/lejito/epharma/model/Prescription.java
+++ src/main/java/com/lejito/epharma/model/Prescription.java
@@ -8,15 +8,13 @@ public class Prescription {
     private Patient patient;
     private Medicine medicine;
     private int quantity;
-    private Doctor prescribedBy;
     private boolean available;
 
-    public Prescription(int id, Patient patient, Medicine medicine, int quantity, Doctor prescribedBy) {
+    public Prescription(int id, Patient patient, Medicine medicine, int quantity) {
         this.id = id;
         this.patient = patient;
         this.medicine = medicine;
         this.quantity = quantity;
-        this.prescribedBy = prescribedBy;
         this.available = true;
     }
 }
